92142b9add79299c19e2dfc7f9d63ea4242d63f0
npuKoJlucTbl4 <159910054+npuKoJlucTbl4@users.noreply.github.com>
2024-02-29 09:57:52 +0600

    button created
    viruses added(its a joke)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void onMyButtonClick(View view)
+    {
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        Button btn = (Button) findViewById(R.id.button);
+        // выводим сообщение
+        Toast.makeText(this, "downloading Trojan", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "downloading WinLocker", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "downloading NutCracker", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "downloading MLP480p.mp4", Toast.LENGTH_SHORT).show();
+        txt.setText("Enjoy ur virusez");
+        btn.setText("loser");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_blank);
+
     }
 }
